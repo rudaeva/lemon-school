fcc619a4925d6e41e45c04566f14517e02406ecf
rudaeva <sss_olya@mail.ru>
2017-04-28 02:32:43 +0300

    lastmogo

--- MoGo/js/carousel.js
+++ MoGo/js/carousel.js
@@ -1,10 +1,20 @@
 $('.wrapper__carousel').slick({
 	autoplay: true
-	
 })
 // $ ('.carusel__button')animate(funtion(){
 
+
 $('.min-carousel').slick({
 	arrows:true,
-
+	// prevArrow: '<button type="button" class="slick-prev"></button>',
+	// nextArrow: '<button type="button" class="slick-next"></button>',
 })
+
+// $('.accordion-link').on('click', funtion(){
+	 // $('.arrow-top').css ({
+		// backgroundPosition: '-1881px 0px'
+	 // })
+	// $('.arrow-top').addClass('.arrow-bottom')
+	// $('div').removeClass('arrow-top');
+	// $(this).addClass('.arrow-bottom');
+// })
